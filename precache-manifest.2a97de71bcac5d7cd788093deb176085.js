90ce140e3af1941115a077b4e0160917c02bc3e9
foolmonkey <foolmonkey99@gmail.com>
2020-07-31 18:51:24 -0600

    Updates

---
+++ precache-manifest.2a97de71bcac5d7cd788093deb176085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d878db1c43dfe09f9aae91720dc1ff1d",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "4560f9b8a517b665fadc",
+    "url": "/todo-list/static/css/main.cf94099f.chunk.css"
+  },
+  {
+    "revision": "9f3e1d91df6bdf7e4266",
+    "url": "/todo-list/static/js/2.85e3cb6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.85e3cb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4560f9b8a517b665fadc",
+    "url": "/todo-list/static/js/main.6870d31b.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
